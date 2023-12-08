3de8a59f02459f1f12b297950d0d66625e773a90
Erik Jensson <erik@jensson.no>
2023-12-08 21:48:29 +0100

    day3 - part1 almost done

--- day3/main.go
+++ day3/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 
-    //var p1 int = 0;
+	//var p1 int = 0;
 
 	file, err := os.ReadFile("input")
 	if err != nil {
@@ -19,27 +19,34 @@ func main() {
 	line_length := bytes.IndexByte(file, '\n')
 	input := make([]byte, len(file))
 	copy(input, file)
-    
-    var num string
+
+	var num string
 	for i, byte := range file {
 		if unicode.IsNumber(rune(byte)) {
-            num += string(byte)
-            continue
-        }
-        nbh_index_list := get_nbh_index_list(i-len(num)+1, line_length, len(num), input)
-        num = ""
-        fmt.Println(nbh_index_list)
+			num += string(byte)
+			continue
+		}
+		var nbh_index_list []int
+		if num != "" {
+			nbh_index_list = get_nbh_index_list(i-len(num), line_length, len(num), input)
+		    fmt.Println(num, nbh_index_list)
+		}
+		num = ""
 	}
 }
 
 func get_nbh_index_list(start_index int, line_length int, num_length int, input []byte) []int {
-    nbhs := 9 + (num_length)*2
+	nbhs := 9 + (num_length)*3
 	nbh_index_list := make([]int, nbhs)
 	for i := 0; i < nbhs; i++ {
-        prefix := int(math.Floor(float64(i/(3))) - 1)
-        fmt.Println(prefix)
-        postfix := start_index + prefix + prefix*line_length + prefix*(num_length+2)
-		nbh_index_list[i] = postfix
+		y := int(math.Floor(float64(i/(3+num_length))) - 1)
+		x := i%(nbhs/3) - 1
+		if y == 0 && -1 < x && x < num_length {
+			nbh_index_list[i] = -1
+			continue
+		}
+		final_index := start_index + y*line_length + x
+		nbh_index_list[i] = final_index
 	}
 	return nbh_index_list
 }
