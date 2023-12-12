95691188a606a0d6be11ad2adadf50259a2af27c
Erik Jensson <erik@jensson.no>
2023-12-12 14:14:31 +0100

    need to rewrite

--- day3/main.go
+++ day3/main.go
@@ -6,6 +6,7 @@ import (
 	"math"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -23,7 +24,9 @@ func main() {
 	input := make([]byte, len(file))
 	copy(input, file)
 
+	const symbols = "*+$-/@%#=&"
 	var num string
+
 	for i, byte := range file {
 		if unicode.IsNumber(rune(byte)) {
 			num += string(byte)
@@ -32,12 +35,13 @@ func main() {
 		var nbh_index_list []int
 		if num != "" {
 			nbh_index_list = get_nbh_index_list(i-len(num), line_length, len(num), input)
+			fmt.Println(nbh_index_list)
 		}
 		input_length := len(input)
 		for _, nbh_index := range nbh_index_list {
 			if input_length > nbh_index && nbh_index > 0 {
 				fmt.Println(nbh_index, string(input[nbh_index]))
-				if string(input[nbh_index]) != "." && !unicode.IsNumber(rune(input[nbh_index])) {
+				if strings.ContainsAny(string(input[nbh_index]), symbols) {
 					n, err := strconv.Atoi(num)
 					if err != nil {
 						continue
@@ -53,11 +57,14 @@ func main() {
 	fmt.Println("Part 1:", p1)
 }
 
+// TODO: The neighborhood around points on the edge get wrapped around to the next/previous line.
+//
+//	I need a (x,y) abstraction then convert this to appropiate index, if I want to do it this way.
 func get_nbh_index_list(start_index int, line_length int, num_length int, input []byte) []int {
-	nbhs := 9 + (num_length)*3
+	nbhs := 9 + (num_length-1)*3
 	nbh_index_list := make([]int, nbhs)
 	for i := 0; i < nbhs; i++ {
-		y := int(math.Floor(float64(i/(3+num_length))) - 1)
+		y := int(math.Floor(float64(i/(2+num_length))) - 1)
 		x := i%(nbhs/3) - 1
 		if y == 0 && -1 < x && x < num_length {
 			nbh_index_list[i] = -1
