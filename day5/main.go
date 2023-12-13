01c4e48bf3eef2bd19b5c2a20663943126fcba79
Erik Jensson <erik@jensson.no>
2023-12-13 14:33:28 +0100

    something is wrong

--- day5/main.go
+++ day5/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	//var p1 int = 0
+	var p1 int = 0
 
 	file, err := os.Open("input")
 	if err != nil {
@@ -18,11 +18,11 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	//m1 := make(map[string]bool)
+	m1 := make(map[int]int)
 
 	var seeds []string
 	var seeds_done bool = false
-	var seed_soil_start, soil_fert_start, fert_water_start, water_light_start, light_temp_start, temp_humid_start, humid_location_start int
+	start_indexes := make([]int, 7)
 	var lines []string
 
 	// seed-soil-fertilizer-water-light-temperature-humidity-location
@@ -36,25 +36,25 @@ func main() {
 			seeds_done = true
 		}
 		if strings.Contains(line, "seed-to-soil") {
-			seed_soil_start = line_number + 1
+			start_indexes[0] = line_number + 1
 		}
 		if strings.Contains(line, "soil-to-fertilizer") {
-			soil_fert_start = line_number + 1
+			start_indexes[1] = line_number + 1
 		}
 		if strings.Contains(line, "fertilizer-to-water") {
-			fert_water_start = line_number + 1
+			start_indexes[2] = line_number + 1
 		}
 		if strings.Contains(line, "water-to-light") {
-			water_light_start = line_number + 1
+			start_indexes[3] = line_number + 1
 		}
 		if strings.Contains(line, "light-to-temperature") {
-			light_temp_start = line_number + 1
+			start_indexes[4] = line_number + 1
 		}
 		if strings.Contains(line, "temperature-to-humidity ") {
-			temp_humid_start = line_number + 1
+			start_indexes[5] = line_number + 1
 		}
 		if strings.Contains(line, "humidity-to-location") {
-			humid_location_start = line_number + 1
+			start_indexes[6] = line_number + 1
 		}
 
 		if line_number > 3 {
@@ -63,25 +63,49 @@ func main() {
 		line_number += 1
 	}
 
+    var lowest int
 	for _, seed := range seeds {
-		i := 0
-        var seed_to_soil_code int;
 		seed_num, _ := strconv.Atoi(seed)
+		var dest_node int = seed_num
+		j := 0
 		for {
-			line := lines[seed_soil_start+i]
-			s2s := strings.Fields(line)
-			var seed_to_soil []int
-			for i, s := range s2s {
-				seed_to_soil[i], _ = strconv.Atoi(s)
+            if j == len(start_indexes) {
+                break
+            }
+			i := 0
+			for {
+			    line := lines[start_indexes[j]+i]
+                if start_indexes[j]+i > len(lines)-1 {
+                    break
+                }
+                if j < len(start_indexes)-1 {
+                   if start_indexes[j] + i > start_indexes[j+1] - 2 {
+                        break
+                    }
+                }
+				fields := strings.Fields(line)
+                dest_src_range := make([]int,3)
+				for i, s := range fields {
+					dest_src_range[i], _ = strconv.Atoi(s)
+				}
+				if dest_src_range[1] < dest_node && dest_node < dest_src_range[1]+dest_src_range[2] {
+					dest_node = dest_node + (dest_src_range[0] - dest_src_range[1])
+					break
+				}
+				i++
 			}
-			if seed_to_soil[0] < seed_num && seed_num < seed_to_soil[0]+seed_to_soil[2] {
-               seed_to_soil_code = seed_to_soil[1] + (seed_num - seed_to_soil[0])
-               break
-			}
-
-			i++
+			j++
 		}
+        m1[dest_node] = dest_node
+        lowest = dest_node
 	}
+    for _, v := range m1 {
+        if v < lowest {
+            lowest = v
+        }
+    }
+    p1 = lowest
+    fmt.Println(m1)
+    fmt.Println("Part 1:",p1)
 
-	fmt.Println(seeds)
 }
